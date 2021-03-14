93089ce49c1433ddbba3ad91cb627670b433d687
yuya <yuuchin1999@gmail.com>
2021-03-15 00:59:53 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+zenn Next.js と Tailwind を利用したエンジニア名刺アプリ
+https://zenn.dev/yamakenji24/articles/2921714d4b69d0
---
+++ next.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    GITHUB_CLIENT: process.env.GITHUB_CLIENT,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    GOOGLE_CLIENT: process.env.GOOGLE_CLIENT,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+  },
+  images: {
+    domains: [
+      "avatars.githubusercontent.com",
+      "avatars0.githubusercontent.com",
+      "avatars1.githubusercontent.com",
+      "avatars2.githubusercontent.com",
+      "avatars3.githubusercontent.com",
+      "avatars4.githubusercontent.com",
+      "grass-graph.moshimo.works",
+      "lh3.googleusercontent.com",
+      "lh4.googleusercontent.com",
+      "lh5.googleusercontent.com",
+      "lh6.googleusercontent.com",
+      "github.com",
+    ],
+  },
+};
